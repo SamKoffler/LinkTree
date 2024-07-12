02d4019df9cdc0949fbf478e50632e6555ae6e6b
SamKoffler <smjobob@gmail.com>
2024-07-12 16:42:21 -0400

    Update streamlit_app.py
    Personal changes 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,14 @@ col2.image(Image.open('dp.png'))
 
 st.header('Samuel Koffler, Undergrad')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('Student at Michigan State University, focused in Computer Science with an interest in Chemistry and Pharmacology')
 
 icon_size = 20
 
 st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
+st_button('youtube', 'https://www.youtube.com/@CereaLover123', 'Coding Professor YouTube channel', icon_size)
 st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
+st_button('twitter', 'https://x.com/SamuelKoffler', 'Follow me on X', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/samuel-koffler-3216b0290/', 'Follow me on LinkedIn', icon_size)
 st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
