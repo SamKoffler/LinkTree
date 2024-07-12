2ede82d65337e77f7d2dce4cc7cd855576e74155
SamKoffler <smjobob@gmail.com>
2024-07-12 16:51:18 -0400

    Update streamlit_app.py
    Personal 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ st.info('Student at Michigan State University, focused in Computer Science with 
 
 icon_size = 20
 
-st_button('youtube', 'https://www.youtube.com/@CereaLover123', 'DoodeleJump Speedrunning YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/playlist?list=PL6jgA9JKEkMHi8SsHUa1-qOeUhADCBwwW&si=IlNnJLWHAp2PYCd2', 'My Reference Youtube Videos Playlist', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
+st_button('youtube', 'https://www.youtube.com/@CereaLover123', 'My YouTube channel', icon_size)
+st_button('youtube', 'https://youtube.com/playlist?list=PL6jgA9JKEkMHi8SsHUa1-qOeUhADCBwwW&si=IlNnJLWHAp2PYCd2', 'My reference YouTube videos playlist', icon_size)
+#st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
 st_button('twitter', 'https://x.com/SamuelKoffler', 'Follow me on X', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/samuel-koffler-3216b0290/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+#st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
+#st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
