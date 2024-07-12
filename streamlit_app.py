17d7a89e953b164b8d6074d34eabe2ce22ed756f
SamKoffler <smjobob@gmail.com>
2024-07-12 17:05:24 -0400

    Update streamlit_app.py
    Personal 7

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,12 @@ st.info('Student at Michigan State University, focused in Computer Science with 
 
 icon_size = 20
 
+
+st_button('cup', 'https://github.com/SamKoffler', 'My Github', icon_size)
 st_button('youtube', 'https://www.youtube.com/@CereaLover123', 'Sub to my YouTube channel', icon_size)
 #st_button('youtube', 'https://youtube.com/playlist?list=PL6jgA9JKEkMHi8SsHUa1-qOeUhADCBwwW&si=IlNnJLWHAp2PYCd2', 'My reference YouTube videos playlist', icon_size)
 st_button('medium', 'https://www.instagram.com/sam_koffler_/', 'Follow me on Instagram', icon_size)
 st_button('twitter', 'https://x.com/SamuelKoffler', 'Follow me on X', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/samuel-koffler-3216b0290/', 'Follow me on LinkedIn', icon_size)
 st_button('newsletter', 'https://www.facebook.com/sam.koffler.58/', 'Friend me on Facebook', icon_size)
-#st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+
